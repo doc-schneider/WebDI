800c776f2fee82b0b0f0b9b712b12c2d6af00c42
Stefan <docschneider1@web.de>
2021-02-12 21:37:36 +0100

    Box class

---
+++ Views/Box.py
@@ -0,0 +1,74 @@
+import pandas as pd
+
+from DataOperations.Utilities import add_thumbnail_to_filename
+from Views.Utilities import list_column_to_str, encode_data
+from Views.Parent import Viewer
+
+
+class BoxViewer(Viewer):
+    def __init__(self, View):
+        super().__init__(View.document_pathtype, View.encode_type)
+
+    # photoTimeline specifics: Time, photo information
+    def init_photoTimeline(self, documenttable, time_interval, use_thumbnail):
+        self.use_thumbnail = use_thumbnail
+        self.update_photoTimeline(documenttable, time_interval)
+
+    def update_photoTimeline(self, documenttable, time_interval):
+        self.timeinterval = pd.Interval(time_interval['TIME_FROM'].iloc[0],
+                                   time_interval['TIME_TO'].iloc[0], closed='left')
+        self.index_documents = documenttable.find_in_timeinterval(self.timeinterval)
+        self.update(documenttable)
+        if self.use_thumbnail:
+            # TODO Use apply
+            for i in range(self.boxShow.shape[0]):
+                if self.boxShow['LOCATION_DOCUMENT'].iloc[i]:
+                    self.boxShow.loc[i, 'LOCATION_DOCUMENT'] = add_thumbnail_to_filename(
+                        self.boxShow['LOCATION_DOCUMENT'].iloc[i],
+                        self.boxShow['DOCUMENT_TYPE'].iloc[i][0]
+                    )
+
+    # General update method for BoxView
+    def update(self, documenttable):
+        if 'GROUP_INDEX' in documenttable.data:
+            self.index_groups = documenttable.data.loc[self.index_documents, 'GROUP_INDEX'].unique().tolist()
+        else:
+            self.index_groups = None
+        self.select_show()
+        # Dataframe holding the current document to show
+        if self.index_groups:
+            self.boxShow = documenttable.data.loc[documenttable.data['GROUP_INDEX']==self.index_show, ].copy()
+        else:
+            self.boxShow = documenttable.data.loc[self.index_show, ].copy()
+        self.document_location()
+        # Convert lists to string or None
+        # TODO Multiple elemts of Description list
+        self.boxShow['DOCUMENT_TYPE'] = self.boxShow['DOCUMENT_TYPE'].apply(list_column_to_str)
+        self.boxShow['DESCRIPTION'] = self.boxShow['DESCRIPTION'].apply(list_column_to_str)
+
+    # Which document to show in box. Default: first.
+    def select_show(self, which=0):
+        self.index_show = []
+        if self.index_groups is not None:
+            if self.index_groups:
+                self.index_show = self.index_groups[which]
+        else:
+            if self.index_documents:
+                self.index_show = self.index_documents[which]
+
+    # Complete file location
+    def document_location(self):
+        # Absolute file path
+        if self.document_pathtype == 'PATH':
+            self.boxShow['LOCATION_DOCUMENT'] = self.boxShow['PATH'] + '/' + self.boxShow['DOCUMENT_NAME']
+            self.boxShow.loc[self.boxShow['PATH']=='', 'LOCATION_DOCUMENT'] = None
+
+    ## Methods for website viewing
+
+    # Return list of descriptions
+    def descriptions(self):
+        return self.boxShow['DESCRIPTION'].tolist()
+
+    # Load data from file and return base64 for viewing in website
+    def encode_data(self):
+        return self.boxShow['LOCATION_DOCUMENT'].apply(encode_data, args=(self.encode_type,)).tolist()
