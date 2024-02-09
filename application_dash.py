3e6c46e263e53f80a1dcd9ffd9ca976da9d15e4f
Doc Schneider <docschneider1@web.de>
2024-02-09 22:11:32 +0100

    Test app

--- application_dash.py
+++ application_dash.py
@@ -1,26 +1,6 @@
 import dash
 from dash import Dash, html, dcc
-from sqlalchemy import create_engine, MetaData
 
-from DataOperations import TableOperations
-from DataStructures.TableTypes import TableType
-import config
-
-
-config.person = "stefan"
-# MySQL
-db_connection_str = 'mysql+mysqlconnector://root:Moppel3!@localhost/lives'
-db_engine = create_engine(db_connection_str)
-db_conn = db_engine.connect()
-metadata = MetaData()
-metadata.reflect(bind=db_engine)
-config.mysql = {
-    "engine": db_engine,
-    "conn": db_conn,
-    "metadata": metadata
-}
-# View Table: Start with Topic / highest level
-#config.table_view = TableOperations.init_table(TableType["TOPIC"], config.person)
 
 app = Dash(__name__, use_pages=True, suppress_callback_exceptions=True)
 app.layout = html.Div([
